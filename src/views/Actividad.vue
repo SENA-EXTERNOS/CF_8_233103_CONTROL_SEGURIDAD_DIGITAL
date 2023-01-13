6f19c81c32f241dc35aa6587026f018efa00687d
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2023-01-13 10:55:19 -0500

    uup

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,10 +7,10 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/arrastrar-.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Arrastrar y soltar
+          h2 Ciberseguridad - Impacto de eventos e incidentes
         p.mb-3 Esta actividad le permitirá determinar el grado de apropiación de los contenidos del componente formativo.
         p.mb-5 De acuerdo con la definición planteada en la columna izquierda, arrastre cada término al lugar que considere correcto de la columna derecha.
  
@@ -19,7 +19,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
+              a.boton.boton--b.fnd-y(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
