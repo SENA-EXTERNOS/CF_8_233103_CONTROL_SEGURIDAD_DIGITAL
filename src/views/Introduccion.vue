2760aec716f29d6f7f19531262b4e5f3d596cb7e
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-30 20:32:08 -0500

    Se vinculan los dos videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,7 +23,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/9lyRIrGUaHI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
                
 
 </template>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -90,7 +90,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/EOfiuGk1smA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
                
     h3 Plataformas de seguridad
 
